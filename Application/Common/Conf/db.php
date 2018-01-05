d4ab18567f45d866e3565336c570b6302d39264a
iAsuma <770878450@qq.com>
2018-01-05 10:25:51 +0800

    conf

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -1,10 +1,10 @@
 <?php
 return array(
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => 'localhost', // 服务器地址
-    'DB_NAME' => 'echobug', // 数据库名
-    'DB_USER' => 'root', // 用户名
-    'DB_PWD' => 'root', // 密码
+    'DB_HOST' => 'bdm271635283.my3w.com', // 服务器地址
+    'DB_NAME' => 'bdm271635283_db', // 数据库名
+    'DB_USER' => 'bdm271635283', // 用户名
+    'DB_PWD' => 'lsq890927', // 密码
     'DB_PORT' => 3306, // 端口
     'DB_PREFIX' => '', // 数据库表前缀
     'DB_CHARSET' => 'utf8', // 字符集
